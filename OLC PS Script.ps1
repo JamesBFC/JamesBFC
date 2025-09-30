22091b1a7fc53efca769dff93f29b6ce76820d26
JamesBFC <james.gale@bracknell-forest.gov.uk>
2025-09-30 13:52:01 +0100

    Add files via upload

--- OLC PS Script.ps1
+++ OLC PS Script.ps1
@@ -1,12 +1,18 @@
 $url = "https://bracknellforestcouncil.bksblive2.co.uk/bksbLive2/Login.aspx?ReturnUrl"
 $name = "OneAdvanced Online Learning.url"
 $shell = New-Object -ComObject WScript.Shell
-$desktop = [System.Environment]::GetFolderPath("Desktop")
-$shortcutPath = "$desktop\$name"
+
+# Get the path to the Public Desktop
+# Environment.SpecialFolder::CommonDesktop is the .NET way to get the Public Desktop path
+$publicDesktop = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::CommonDesktopDirectory)
+
+$shortcutPath = "$publicDesktop\$name"
 
 # Create a basic internet shortcut file
+# Note: For internet shortcuts (.url), the file is a simple text file, 
+# but using the Shell object is a reliable way to create it.
 $shortcut = $shell.CreateShortcut($shortcutPath)
 $shortcut.TargetPath = $url
 $shortcut.Save()
 
-Write-Host "Web link shortcut created: $shortcutPath"
+Write-Host "Web link shortcut created on Public Desktop: $shortcutPath"
